5ee116df6157d3d88e53c330251e5dbb012c5008
shyynux <shyynux@gmail.com>
2023-08-28 23:37:26 +0530

    add signup button

--- authentication.js
+++ authentication.js
@@ -5,12 +5,9 @@ const popupOverlay = document.createElement("div");
 popupOverlay.classList.add("popup-overlay");
 
 /* the login button was pressed */
-
 const loginButton = document.getElementById("login");
 
 loginButton.addEventListener("click", function(){
-    /* Redirect the user to the login.html page */
-    // window.location.href = 'login.html';
 
     /* a pop up will open that will log the user in */
     // Create the popup content
@@ -58,4 +55,86 @@ function createLoginPopup(){
     popupContent.appendChild(loginPopupButton);
 
     return popupContent;
+}
+
+/* the sign up button */
+
+const signupButton = document.getElementById("signup");
+let signupUsername;
+let signUpPassword;
+let signUpPassword2;
+let signupCreateAccountbutton;
+let passwordMismatchErrorLabel;
+
+/* the signup button was pressed */
+signupButton.addEventListener("click", function(){
+
+    /* a pop up will open that will let the user sign up */
+    const popupContent = createSignupPopup();
+    console.log("the Signup button was clicked and pop-up created.")
+
+     popupOverlay.innerHTML = "";
+     popupOverlay.appendChild(popupContent);
+     document.body.appendChild(popupOverlay);
+     popupOverlay.style.display = "flex";
+
+     /* the user enters the inputs */
+     signupCreateAccountbutton.addEventListener("click", function(){
+        if (signUpPassword.value !== signUpPassword2.value) {
+            passwordMismatchErrorLabel.textContent = "Passwords do not match.";
+          } else {
+            passwordMismatchErrorLabel.textContent = ""; // Clear the error message
+          }
+     });
+
+    /* click the close button */
+     /* Close the popup */
+    //  popupOverlay.style.display = "none";
+});
+
+/* create the signup popup */
+function createSignupPopup(){
+      /* a signup button as well */
+      const popupContent = document.createElement("div");
+      popupContent.classList.add("popup-content");
+  
+     /* username, email-id,
+     choose password, retype password, city where your mom went to school */
+
+      const username = document.createElement("input");
+      username.type = "text";
+      username.placeholder = "Choose a unique username";
+      username.id = "user-name";
+    
+      const password = document.createElement("input");
+      password.type = "password";
+      password.placeholder = "Pick a strong password.";
+      password.id = "new-password";
+
+      const retypePassword = document.createElement("input");
+      retypePassword.type = "password";
+      password.placeholder = "Please retype it.";
+      password.id = "retyped-password";
+  
+      const finalSignupButton = document.createElement("button");
+      finalSignupButton.textContent = "Create account";
+      finalSignupButton.classList.add("button-container");
+
+      const passwordMismatchError = document.createElement("label");
+      passwordMismatchError.id = "passwordMismatchError";
+
+      passwordMismatchErrorLabel = passwordMismatchError;
+
+      signupUsername = username;
+      signUpPassword = password;
+      signUpPassword2 = retypePassword;
+      signupCreateAccountbutton = finalSignupButton;
+  
+      popupContent.appendChild(username);
+      popupContent.appendChild(password);
+      popupContent.appendChild(retypePassword);
+      popupContent.appendChild(passwordMismatchError);
+      popupContent.appendChild(finalSignupButton);
+  
+      return popupContent;
 }
